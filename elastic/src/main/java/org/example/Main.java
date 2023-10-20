1282cca2d9b65f6ede04c0d0964dc6b5c457b87d
matafiks <62651471+matafiks@users.noreply.github.com>
2023-10-20 10:39:12 +0200

    pobranie kursu euro z nbp

--- elastic/src/main/java/org/example/Main.java
+++ elastic/src/main/java/org/example/Main.java
@@ -1,7 +1,44 @@
 package org.example;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        OkHttpClient client = new OkHttpClient();
+        String apiUrl = "https://api.nbp.pl/api/exchangerates/rates/A/EUR/?format=json"; // Euro exchange rate for today
+
+        Request request = new Request.Builder()
+                .url(apiUrl)
+                .get()
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+                JsonObject jsonResponse = JsonParser.parseString(responseBody).getAsJsonObject();
+
+                String table = jsonResponse.get("table").getAsString();
+                String currency = jsonResponse.get("currency").getAsString();
+                String code = jsonResponse.get("code").getAsString();
+                String effectiveDate = jsonResponse.get("rates").getAsJsonArray().get(0).getAsJsonObject().get("effectiveDate").getAsString();
+                double rate = jsonResponse.get("rates").getAsJsonArray().get(0).getAsJsonObject().get("mid").getAsDouble();
+
+                //System.out.println("Tabela: " + table);
+                System.out.println("Waluta: " + currency);
+                System.out.println("Kod: " + code);
+                System.out.println("Data: " + effectiveDate);
+                System.out.println("Kurs średni: " + rate);
+            } else {
+                System.err.println("Błąd: " + response.code() + " - " + response.message());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
